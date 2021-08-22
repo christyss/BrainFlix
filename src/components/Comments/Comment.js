2d815d1a55df14850b67b5518f8d78ccae69725c
christyss <christy.suenmw@gmail.com>
2021-08-22 01:24:55 -0400

    finished styling mobile

--- src/components/Comments/Comment.js
+++ src/components/Comments/Comment.js
@@ -6,15 +6,17 @@ import React from 'react';
 function Comment(props) {
     return (
         <article>
-            <form>
-                <h2>Comments</h2>
-                <div>
-                    <img className="comment__user-icon" src="UserIcon" alt="Icon"/>
-                </div>
-                <div>
-                    <h3>JOIN THE CONVERSATION</h3>
-                    <textarea type="text" name="input" placeholder="Write comment here"/>
-                    <button>COMMENT</button>
+            <form className="comment-form">
+                <h2 className="comment-form__title">{}Comments</h2>
+                <div className="comment-form__user-input">
+                    <div className="comment-form__user-input comment-form__user">
+                        <img className="comment-form__user-icon" src={UserIcon} alt="Icon"/>
+                    </div>
+                    <div className="comment-form__user-input comment-form__input">
+                        <h3 className="comment-form__input-title">JOIN THE CONVERSATION</h3>
+                        <textarea className="comment-form__input-area" type="text" name="input" placeholder="Write comment here"/>
+                        <button className="comment-form__input-btn">COMMENT</button>
+                    </div>
                 </div>
             </form>
             <section className="allComments">
--- src/components/Video/VideoOnPlay.js
+++ src/components/Video/VideoOnPlay.js
@@ -6,29 +6,29 @@ function VideoOnPlay(props) {
     const video = props.onPlayVideo
     return (
         <section className="video-container">
+            <div className="video-container__video-onplay">
                 <video key={video.id} className="video-container__video" controls src={video.video} poster={video.image}></video>
-            <div>
+            </div>
+            <div className="video-container__video-details">
                 <h1 className="video-container__title">{video.title}</h1>
-            <div>
-                <h2 className="video-container__channel">By {video.channel}</h2>
-                <h2 className="video-container__date">{video.timestamp}</h2>
+            <div className="video-container__info">
+                <h2 className="video-container__info-channel">By {video.channel}</h2>
+                <h2 className="video-container__info-date">{video.timestamp}</h2>
             </div>
-            <div>
-            <div>
-                <div className="video-container__views">
-                <img className="video-container__views-icon" src={View} alt="Views" />
-                <h2 className="video-container__views-number">{video.views}</h2>
+            <div className="video-container__views-likes">
+                <div className="video__views">
+                    <img className="video__views-icon" src={View} alt="Views" />
+                    <h2 className="video__views-number">{video.views}</h2>
+                </div>
+                <div className=" video__likes">
+                    <img className="video__likes-icon" src={Like} alt="Likes" />
+                    <h2 className="video__likes-number">{video.likes}</h2>
+                </div>
             </div>
-            <div className="video-container__likes">
-                <img className="video-container__likes-icon" src={Like} alt="Likes" />
-                <h2 className="video-container__likes-number">{video.likes}</h2>
             </div>
-            </div>
-            </div>
-            <article className="video-container__description">
-                <p className="video__description">{video.description}</p>
-            </article>
-            </div>
+                <article className="video-container__description">
+                    <p className="video-container__description-text">{video.description}</p>
+                </article>
         </section>
     )}
 
