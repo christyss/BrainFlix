18790bd2555167d1f69dd542e87d745748e82751
christyss <christy.suenmw@gmail.com>
2021-09-01 17:30:06 -0400

    second commit

--- src/Utils/API-KEY.js
+++
@@ -1,9 +0,0 @@
-export const API_KEY = '38f093f4-b3b3-4180-9165-7131210bab55'
-export const API_VIDEO = 'https://project-2-api.herokuapp.com/videos';
-// export const API_VIDEO_DETAILS = 'https://project-2-api.herokuapp.com/videos/:id';
-
-
-
-//Diving Deeper
-export const API_COMMENT = 'https://project-2-api.herokuapp.com/videos/:id/comments';
-export const API_DELETE = 'https://project-2-api.herokuapp.com/videos/:videoId/comments/:commentId';
