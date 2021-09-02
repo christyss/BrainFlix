a79cc896a994c73a8843014b65eb981bbeb38132
christyss <christy.suenmw@gmail.com>
2021-09-01 23:30:47 -0400

    finishing the server, working on video post

--- sprint-three/server/index.js
+++ sprint-three/server/index.js
@@ -1,17 +1,29 @@
 const express = require('express');
-const router = express.Router();
-const videoRoute = require('./routes/videos')
-console.log(videoRoute);
+const cors = require('cors');
+const videoRoute = require('./routes/videos');
 
 const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.use(express.static('images'));
+
+app.use(cors());
 
 app.use((req, res, next) => {
-    console.log("hello");
     next();
-})
+});
+
+app.use((req, res, next) => {
+    if(req.method === 'POST' && req.headers['content-type'] !== 'application/json'){
+        return res.status(400)
+    }
+    next();
+});
 
 app.use('/videos', videoRoute);
 
-app.listen(8000, () => {
+app.listen(port, () => {
     console.log("here's the port");
 })
--- sprint-three/server/routes/videos.js
+++ sprint-three/server/routes/videos.js
@@ -1,21 +1,66 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const uniqid = require("uniqid");
+
+const videoPath = "./data/video-details.json";
+
+const readVideos = () => {
+    const fileContent = fs.readFileSync(videoPath);
+    const parseFileContent = JSON.parse(fileContent);
+    return parseFileContent;
+}
+
+const writeVideoInfo = (data) => {
+    const videoContent = JSON.stringify(data); 
+    fs.writeFileSync(videoPath, videoContent);
+}
 
 router.get('/', (_req, res) => {
     try{
-        const fileContent = fs.readFileSync('./data/video-details.json');
-        const parseFileContent = JSON.parse(fileContent);
-
-        return res.status(200).json(parseFileContent); 
+        const videos = readVideos();
+        return res.status(200).json(videos); 
     }catch(err){
         return res.status(500).json({error: "File cannot be read."})
     }
 });    
 
-// router.get('/', (req, res) => {
-//     res.send
-// })
+router.get('/:id', (req, res) => {
+    const videos = readVideos();
+    const singleVideo = videos.find((video) => video.id === req.params.id);
 
+    if(!singleVideo){
+        return res.status(404).send("Video not found!")
+    }
+    res.json(singleVideo);
+}); 
 
-module.export = router;
+router.post('/', (req, res) => {
+    
+    const videos = readVideos();
+    
+    const newVideo = {
+        id: uniqid(),
+        title: "", //req.title
+        channel: "Christy",
+        image: "image",
+        description: "",
+        view: "1M",
+        likes:"8,952",
+        duration: "5:35",
+        video: "video",
+        timestamp: Date.now(),
+        comments: []
+    }
+    videos.push(newVideo);
+    writeVideoInfo(videos);
+    
+    try{
+       return res.status(201).json(newVideo); 
+    }catch{
+        return res.status(404).send("Error")
+    }
+    
+});
+
+module.exports = router;
